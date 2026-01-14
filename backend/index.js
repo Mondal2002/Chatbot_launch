df08da489d7396f26575ae9868a00d5c3a883dd0
Mondal2002 <subhammondal735@gmail.com.com>
2026-01-14 17:55:05 +0530

    thirtythird commit integrating mic functions using assemblyAI

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { Resend } from "resend";
-import speechRoutes from "./routes/speechRoutes.js"
+import speechRoutes from "./components/speechRoutes.js"
 
 dotenv.config();
 
