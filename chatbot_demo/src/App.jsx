1d6ed7581e5d0bce5c8bb6e2e2b9e8c1efc125b3
Mondal2002 <subhammondal735@gmail.com.com>
2026-01-15 15:01:00 +0530

    Fixed copyright

--- chatbot_demo/src/App.jsx
+++ chatbot_demo/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom';
 import Home from './pages/home';
-import Coverpage from './pages/coverpage';
-import ChatAssistant from './components/coffee_chatbot';
-import Speech_check from './components/speech_check';
 
 export const App = () => {
   return (
