fdb67334fc3798f24fc0bc310d751eb1836fee94
Mondal2002 <subhammondal735@gmail.com.com>
2026-03-20 07:47:58 +0530

    Revert back to smallest ai 3

--- chatbot_demo/src/components/coffee_chatbot.jsx
+++ chatbot_demo/src/components/coffee_chatbot.jsx
@@ -201,7 +201,7 @@ const ChatAssistant = () => {
       }
 
       const response = await fetch(
-        "https://chatbot-launch.onrender.com/api/smallai/tts",
+        "https://chatbot-launch.onrender.com/api/tts",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
